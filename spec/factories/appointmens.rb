b1ab6fbd98a0a3862657afa03e3c16fb2113a86c
Alejandro Toledo <atoledofr@gmail.com>
2021-05-03 11:15:39 -0500

    add test cases for appointments resquest

--- spec/factories/appointmens.rb
+++ spec/factories/appointmens.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :appointment do
-    user_id { 1 }
-    restaurant_id { 1 }
+    user
+    restaurant
     date { Faker::Date.in_date_period(year: 2021, month: 4) }
     duration { %i[15 30 45].sample }
     status { %i[booked unbooked].sample }
