6f006da62519bd3dccc2c2642b92107510719884
Alejandro Toledo <atoledofr@gmail.com>
2021-05-03 11:32:26 -0500

    update test suites and routes for appointments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :appointments
+      resources :appointments, only: %i[create index]
       resources :restaurants
       resources :users, only: [:create]
       post '/login', to: 'users#login'
