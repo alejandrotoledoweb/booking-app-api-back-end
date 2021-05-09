479b4577d47217792ce7e0aad6b2f4f65e2abde6
Alejandro Toledo <atoledofr@gmail.com>
2021-05-08 22:50:35 -0500

    modify methods for Restaurants controller and delete comments in appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,29 +1,24 @@
 # app/controllers/items_controller.rb
 class AppointmentsController < ApplicationController
-  # GET /todos/:todo_id/items
   def index
     @appointments = current_user.appointments
     json_response(@appointments)
   end
 
-  # GET /todos/:todo_id/items/:id
   def show
     json_response(@appointment)
   end
 
-  # POST /todos/:todo_id/items
   def create
     @appointment = current_user.restaurant.appointments.create!(appointment_params)
     json_response(@appointment, :created) if @appointment.valid?
   end
 
-  # PUT /todos/:todo_id/items/:id
   def update
     @appointment.update(appointment_params)
     head :no_content
   end
 
-  # DELETE /todos/:todo_id/items/:id
   def destroy
     @appointment.destroy
     head :no_content
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,26 +1,30 @@
-class Api::V1::RestaurantsController < ApplicationController
-  skip_before_action :authorized
-  before_action :set_restaurant, only: %i[show update destroy]
-
+class RestaurantsController < ApplicationController
+  # GET /todos
   def index
+    # get current user todos
     @restaurants = Restaurant.all
     json_response(@restaurants)
   end
 
+  # POST /todos
   def create
-    @restaurant = Restaurant.create!(restaurant_params)
+    # create todos belonging to current user
+    @restaurant = Restaurants.create!(restaurant_params)
     json_response(@restaurant, :created)
   end
 
+  # GET /todos/:id
   def show
     json_response(@restaurant)
   end
 
+  # PUT /todos/:id
   def update
     @restaurant.update(restaurant_params)
     head :no_content
   end
 
+  # DELETE /todos/:id
   def destroy
     @restaurant.destroy
     head :no_content
@@ -32,7 +36,4 @@ class Api::V1::RestaurantsController < ApplicationController
     params.permit(:name, :description, :location, :avatar_link)
   end
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-  end
 end
