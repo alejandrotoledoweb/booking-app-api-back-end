d18d0ae6872d127a9c69acbad11123d76b2a6cfe
Alejandro Toledo <atoledofr@gmail.com>
2021-05-09 15:45:11 -0500

    modify appointment controller methods

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,31 +1,41 @@
 class Api::V1::AppointmentsController < ApplicationController
+  before_action :authenticate_request!
   def index
-    @appointments = current_user.appointments
-    json_response(@appointments)
-  end
+    @appointments = @current_user.appointments
 
-  def show
-    json_response(@appointment)
+    render json: appointments, status: :ok
   end
 
   def create
-    @appointment = current_user.restaurant.appointments.create!(appointment_params)
-    json_response(@appointment, :created) if @appointment.valid?
-  end
+    if !@current_user.appointments.find_by(restaurant_id: params[:restaurant_id])
+      user_appointment = @current_user.appointments.create(appointments_params)
+      if user_appointments.save
+        render json: User.find(@current_user.id).restaurants
+      else
+        render json: { message: user_appointments.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { message: 'appointment already taken' }, status: :unprocessable_entity
 
-  def update
-    @appointment.update(appointment_params)
-    head :no_content
+    end
   end
 
   def destroy
-    @appointment.destroy
-    head :no_content
+    user_appointment = @current_user.appointments.find_by(motocycle_id: params[:motocycle_id])
+
+    if user_appointment
+      user_appointment.destroy
+      @appointments = @current_user.restaurants
+      render json: @appointments
+    else
+      render json: { message: "can't find an appointment with the id #{params[:restaurant_id]} " },
+             status: :unprocessable_entity
+    end
   end
 
   private
 
-  def appointment_params
-    params.permit(:user_id, :restaurant_id, :date, :duration, :status)
+  def appointments_params
+    params.permit(:restaurant_id, :user_id, :date, :duration, :status)
   end
 end
--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,30 +1,23 @@
 class Api::V1::RestaurantsController < ApplicationController
-  # GET /todos
   def index
-    # get current user todos
     @restaurants = Restaurant.all
     json_response(@restaurants)
   end
 
-  # POST /todos
   def create
-    # create todos belonging to current user
     @restaurant = Restaurants.create!(restaurant_params)
     json_response(@restaurant, :created)
   end
 
-  # GET /todos/:id
   def show
     json_response(@restaurant)
   end
 
-  # PUT /todos/:id
   def update
     @restaurant.update(restaurant_params)
     head :no_content
   end
 
-  # DELETE /todos/:id
   def destroy
     @restaurant.destroy
     head :no_content
