99c81d5da4bddcf68238d674ff8dd0202004a9f2
Alejandro Toledo <atoledofr@gmail.com>
2021-05-08 22:51:49 -0500

    modify app controller and add other exception hanlder and response files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,42 +1,14 @@
 class ApplicationController < ActionController::API
-  before_action :authorized
-
   include Response
   include ExceptionHandler
 
-  def encode_token(payload)
-    JWT.encode(payload, 's3cr3t')
-  end
+  before_action :authorize_request
+  attr_reader :current_user
 
-  def auth_header
-    request.headers['Authorization']
-  end
+  private
 
-  def decoded_token
-    return false unless auth_header
-
-    token = auth_header.split(' ')[1]
-    begin
-      JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
-    rescue JWT::DecodeError
-      false
-    end
-  end
-
-  def logged_in_user
-    return false unless decoded_token
-
-    user_id = decoded_token[0]['user_id']
-    @user = User.select(:id, :name, :username, :email).find_by(id: user_id)
-  end
-
-  def logged_in?
-    return true if logged_in_user
-
-    false
-  end
-
-  def authorized
-    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  # Check for valid request token and return user
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
   end
 end
--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,3 +1,4 @@
+# app/controllers/concerns/response.rb
 module Response
   def json_response(object, status = :ok)
     render json: object, status: status
