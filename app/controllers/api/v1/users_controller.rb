bc989ca9d3840c674e556244e865db5ed6e08827
Alejandro Toledo <atoledofr@gmail.com>
2021-05-10 15:52:28 -0500

    fix rubocop issues

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+  
   def create
     @user = User.create(user_params)
     if @user.valid?
