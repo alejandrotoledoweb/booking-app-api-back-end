5af4c9bea22cc1df3399b3b777d6cc9ae9ed9e5a
Alejandro Toledo <atoledofr@gmail.com>
2021-05-12 11:50:54 -0500

    modify the user controller condition

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,11 +3,11 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    if @user.valid?
+    if @user.save
       token = encode_token({ user_id: @user.id })
       render json: { user: trim_user(@user), token: token, created: true }, status: :created
     else
-      render json: { created: false, error_messages: @user.errors.full_messages }
+      render json: { created: false, error_messages: @user.errors.full_messages }, status: :not_acceptable
     end
   end
 
