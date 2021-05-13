7b2915df60925123d0f1f25056b96c888e6dd95b
Alejandro Toledo <atoledofr@gmail.com>
2021-05-13 09:56:38 -0500

    reinstall gems and update index appointment method to use jbuilder instead

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'puma', '~> 5.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
-gem 'active_model_serializers', '~> 0.10.0'
+# gem 'active_model_serializers', '~> 0.10.0'
 gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
@@ -22,7 +22,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
-gem 'jbuilder'
+gem 'jbuilder', '~> 2.7'
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,9 +1,6 @@
 class Api::V1::AppointmentsController < ApplicationController
   def index
     @appointments = logged_in_user.appointments
-    return json_response(full_appointments(@appointments)) if @appointments
-
-    error_message
   end
 
   def create
@@ -21,21 +18,5 @@ class Api::V1::AppointmentsController < ApplicationController
 
   def error_message
     render json: { error: 'You have to login.', status: 'NOT_LOGGED_IN' }
-  end
-
-  def full_appointments(appointments)
-    full_appointments = []
-    appointments.each do |app|
-      full_appointments << {
-        date_created: app.created_at,
-        id: app.id,
-        date: app.date,
-        status: app.status,
-        duration: app.duration,
-        restaurant: app.restaurant[:name],
-        location: app.restaurant[:location]
-      }
-    end
-    full_appointments
   end
 end
